482e2771b38b717d1a6171a045a2d9709703237b
Naski1 <gantengiksan65@gmail.com>
2024-12-13 21:55:31 +0700

    pembagian login

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,8 +1,6 @@
 <script setup>
 import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 const form = useForm({
@@ -52,59 +50,48 @@ const submit = () => {
                         class="img-fluid" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                    <form>
-                        <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                            <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-floating mx-1">
-                                <i class="fab fa-facebook-f"></i>
-                            </button>
-
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-floating mx-1">
-                                <i class="fab fa-twitter"></i>
-                            </button>
-
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-floating mx-1">
-                                <i class="fab fa-linkedin-in"></i>
-                            </button>
-                        </div>
-
-                        <div class="divider d-flex align-items-center my-4">
-                            <p class="text-center fw-bold mx-3 mb-0">Or</p>
-                        </div>
+                    <form @submit.prevent="submit">
 
                         <!-- Email input -->
-                        <div data-mdb-input-init class="form-outline mb-4">
-                            <input type="email" id="form3Example3" class="form-control form-control-lg"
-                                placeholder="Enter a valid email address" />
-                            <label class="form-label" for="form3Example3">Email address</label>
+                        <div  class="form-outline mb-4">
+                            <input type="text" id="name" class="form-control form-control-lg"
+                                placeholder="Masukkan Nama lengkap anda" v-model="form.name" required autofocus
+                                autocomplete="name" />
+                            <InputError class="mt-2" :message="form.errors.name" />
+                            <label class="form-label">Nama lengkap</label>
+                        </div>
+
+                        <div  class="form-outline mb-4">
+                            <input type="email" id="email" class="form-control form-control-lg"
+                                placeholder="Enter a valid email address" v-model="form.email" required autofocus autocomplete="username" />
+                            <InputError class="mt-2" :message="form.errors.email" />
+                            <label class="form-label">Email address</label>
                         </div>
 
                         <!-- Password input -->
-                        <div data-mdb-input-init class="form-outline mb-3">
-                            <input type="password" id="form3Example4" class="form-control form-control-lg"
-                                placeholder="Enter password" />
-                            <label class="form-label" for="form3Example4">Password</label>
+                        <div  class="form-outline mb-3">
+                            <input type="password" id="password" class="form-control form-control-lg"
+                                placeholder="Enter password" v-model="form.password" required
+                                autocomplete="new-password" />
+                                <InputError class="mt-2" :message="form.errors.password" />
+                            <label class="form-label">Password</label>
                         </div>
 
-                        <div class="d-flex justify-content-between align-items-center">
-                            <!-- Checkbox -->
-                            <div class="form-check mb-0">
-                                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                                <label class="form-check-label" for="form2Example3">
-                                    Remember me
-                                </label>
-                            </div>
-                            <a href="#!" class="text-body">Forgot password?</a>
+                        <div  class="form-outline mb-3">
+                            <input type="password" id="password_confirmation" class="form-control form-control-lg"
+                                placeholder="Konfirmansi password" v-model="form.password_confirmation" required
+                                autocomplete="new-password" />
+                                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                            <label class="form-label">Password</label>
                         </div>
 
                         <div class="text-center text-lg-start mt-4 pt-2">
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-lg"
-                                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+
+                            <PrimaryButton :class="{ 'opacity-25': form.processing }"
+                                :disabled="form.processing">
+                                Register
+                            </PrimaryButton>
+                            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a :href="route('login')"
                                     class="link-danger">Register</a></p>
                         </div>
 
@@ -113,53 +100,4 @@ const submit = () => {
             </div>
         </div>
     </section>
-
-    <form @submit.prevent="submit">
-        <div>
-            <InputLabel for="name" value="Name" />
-
-            <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus
-                autocomplete="name" />
-
-            <InputError class="mt-2" :message="form.errors.name" />
-        </div>
-
-        <div class="mt-4">
-            <InputLabel for="email" value="Email" />
-
-            <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
-                autocomplete="username" />
-
-            <InputError class="mt-2" :message="form.errors.email" />
-        </div>
-
-        <div class="mt-4">
-            <InputLabel for="password" value="Password" />
-
-            <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-                autocomplete="new-password" />
-
-            <InputError class="mt-2" :message="form.errors.password" />
-        </div>
-
-        <div class="mt-4">
-            <InputLabel for="password_confirmation" value="Confirm Password" />
-
-            <TextInput id="password_confirmation" type="password" class="mt-1 block w-full"
-                v-model="form.password_confirmation" required autocomplete="new-password" />
-
-            <InputError class="mt-2" :message="form.errors.password_confirmation" />
-        </div>
-
-        <div class="mt-4 flex items-center justify-end">
-            <Link :href="route('login')"
-                class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
-            Already registered?
-            </Link>
-
-            <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Register
-            </PrimaryButton>
-        </div>
-    </form>
 </template>
